d53b0736fd68a1a21c39093b3c4fe789a82320bb
Evan Strat <evan10s@users.noreply.github.com>
2017-05-23 12:54:00 -0400

    Improvements on time processing (WIP)

--- src/app/participant-form/participant-form.component.ts
+++ src/app/participant-form/participant-form.component.ts
@@ -9,10 +9,10 @@ import * as parseTime from 'parse-loose-time';
     <div class="form-group" [formGroup]="participantForm">
       <label>Participant {{ partNum }}</label>
       <input type="text" placeholder="Name" formControlName="name" />
-      <input type="text" placeholder="Start time" (change)="test()" size="8" formControlName="startTime" />
+      <input type="text" placeholder="Start time" (change)="test(participantForm.value)" size="8" formControlName="startTime" />
       <input type="text" placeholder="End time" size="8" formControlName="endTime" />
       <div class="checkbox-inline">
-        <input type="checkbox" checked="checked" [id]="identifier('bsa')" formControlName="type">
+        <input type="checkbox" checked="checked" [id]="identifier('bsa')" ngFalseValue="Non-registered" ngTrueValue="Registered" ngformControlName="type">
         <label [for]="identifier('bsa')">BSA</label>
       </div>
       <div class="radio-inline">
@@ -44,14 +44,14 @@ export class ParticipantFormComponent implements OnInit {
     this.participantForm = this.toFormGroup(this.participant);
 
     this.participants.push(this.participantForm);
-    // this.participantForm.valueChanges.subscribe(x => {
+    this.participantForm.valueChanges.subscribe(val => {
 
-    // });
+    });
   }
   private toFormGroup(data: Person): FormGroup {
     const formGroup = this.fb.group({
       name: ['', Validators.required],
-      type: true,
+      type: "Registered",
       age: "part_youth",
       startTime: ['', Validators.required],
       endTime: ['', Validators.required],
@@ -59,17 +59,48 @@ export class ParticipantFormComponent implements OnInit {
     })
     return formGroup;
   }
-  test() {
-      const startTime = parseTime(this.participant.startTime) || { hour: 0 };
-      if (startTime.hour <= 6 && this.participant.startTime.length >= 3 && x.startTime.indexOf("am") === -1 && x.startTime.indexOf("a") === -1) {
-        this.participant.startTime += "pm";
-        this.participantForm.patchValue({ startTime: this.participant.startTime });
+  test(info: any) {
+    console.log(info);
+    const startTime = parseTime(info.startTime) || { hour: 0 };
+    console.log("startTime",startTime);
+    if (Object.keys(startTime).length === 2) { //check that start time is a valid time
+      /* Here's the logic for this:
+      * The start time is most likely to be in the morning.  But in some cases, it could also be in the afternoon.
+      * Most start times will be 6-11 AM (inclusive) or 12-5 PM.
+      * For inputs to the start time field that are both a valid time and that do not already have an AM/PM designation, hours 6-11 will have an AM appended, while hours 12-5 will have PM appended.
+      * The end time logic is similar to the above, but with different ranges: 12-7 become PM, 8-11 become AM.
+      * There's also a validation that the end time must be later than the start time.  Time durations that seem illogical (> 8 hours) will also be flagged for manual review by the user.
+      */
+
+      console.log("info.startTime is","" + info.startTime);
+      if (/A{1}M?|P{1}M?/i.test("" + info.startTime)) { //just reformat time
+        console.log("inside");
+        let ampm, adjustedHour = startTime.hour, paddedMinute = startTime.minute;
+        if (startTime.hour >= 12) {
+          ampm = "PM";
+          if (startTime.hour > 12) {
+            adjustedHour -= 12;
+          }
+        } else {
+          ampm = "AM";
+        }
+        if (startTime.minute < 10) {
+          paddedMinute = "0" + paddedMinute;
+        }
+        this.participantForm.patchValue({ startTime: `${adjustedHour}:${paddedMinute} ${ampm}`})
+      } else if (startTime.hour <= 6 || startTime.hour > 12) {
+        this.participantForm.patchValue({ startTime: `${startTime.hour}:${startTime.minute} PM`});
       }
-      console.log("Start time",parseTime(x.startTime));
-      console.log("End time",parseTime(x.endTime));
-      x.totalTime=x.endTime - x.startTime;
-      console.log(x);
-    console.log("participant is", this.participant);
+    }
+    //   if (startTime.hour <= 6 && this.participant.startTime.length >= 3 && x.startTime.indexOf("am") === -1 && x.startTime.indexOf("a") === -1) {
+    //     this.participant.startTime += "pm";
+    //     this.participantForm.patchValue({ startTime: this.participant.startTime });
+    //   }
+    //   console.log("Start time",parseTime(x.startTime));
+    //   console.log("End time",parseTime(x.endTime));
+    //   x.totalTime=x.endTime - x.startTime;
+    //   console.log(x);
+    // console.log("participant is", this.participant);
   }
   identifier(key: string): any {
     return `${key}${this.workdayNum}-${this.partNum}`;
