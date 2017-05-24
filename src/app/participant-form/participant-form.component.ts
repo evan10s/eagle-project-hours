5f9ed0b0e26bac34851c8b5a5b669c20cde7a820
Evan Strat <evan10s@users.noreply.github.com>
2017-05-23 21:42:41 -0400

    Add moment.js and improve time formatter

--- src/app/participant-form/participant-form.component.ts
+++ src/app/participant-form/participant-form.component.ts
@@ -9,8 +9,8 @@ import * as parseTime from 'parse-loose-time';
     <div class="form-group" [formGroup]="participantForm">
       <label>Participant {{ partNum }}</label>
       <input type="text" placeholder="Name" formControlName="name" />
-      <input type="text" placeholder="Start time" (change)="test(participantForm.value)" size="8" formControlName="startTime" />
-      <input type="text" placeholder="End time" size="8" formControlName="endTime" />
+      <input type="text" placeholder="Start time" (change)="prettyTime('start',participantForm.value)" size="8" formControlName="startTime" />
+      <input type="text" placeholder="End time" size="8" (change)="prettyTime('end',participantForm.value)" formControlName="endTime" />
       <div class="checkbox-inline">
         <input type="checkbox" checked="checked" [id]="identifier('bsa')" ngFalseValue="Non-registered" ngTrueValue="Registered" ngformControlName="type">
         <label [for]="identifier('bsa')">BSA</label>
@@ -59,37 +59,92 @@ export class ParticipantFormComponent implements OnInit {
     })
     return formGroup;
   }
-  test(info: any) {
-    console.log(info);
-    const startTime = parseTime(info.startTime) || { hour: 0 };
-    console.log("startTime",startTime);
-    if (Object.keys(startTime).length === 2) { //check that start time is a valid time
-      /* Here's the logic for this:
-      * The start time is most likely to be in the morning.  But in some cases, it could also be in the afternoon.
-      * Most start times will be 6-11 AM (inclusive) or 12-5 PM.
-      * For inputs to the start time field that are both a valid time and that do not already have an AM/PM designation, hours 6-11 will have an AM appended, while hours 12-5 will have PM appended.
-      * The end time logic is similar to the above, but with different ranges: 12-7 become PM, 8-11 become AM.
-      * There's also a validation that the end time must be later than the start time.  Time durations that seem illogical (> 8 hours) will also be flagged for manual review by the user.
-      */
+  timeIncludesAMPM(time: string): boolean {
+    return /A{1}M?|P{1}M?/i.test(time);
+  }
+  updateTimeField(time: Time, field: string): void {
+    let result: object = {};
 
-      console.log("info.startTime is","" + info.startTime);
-      if (/A{1}M?|P{1}M?/i.test("" + info.startTime)) { //just reformat time
-        console.log("inside");
-        let ampm, adjustedHour = startTime.hour, paddedMinute = startTime.minute;
-        if (startTime.hour >= 12) {
-          ampm = "PM";
-          if (startTime.hour > 12) {
-            adjustedHour -= 12;
-          }
-        } else {
-          ampm = "AM";
+    let ampm, adjustedHour = time.hour,
+    paddedMinute: string = "" + time.minute;
+    if (time.hour >= 12) {
+      ampm = "PM";
+      if (time.hour > 12) {
+        adjustedHour -= 12;
+      }
+    } else {
+      ampm = "AM";
+    }
+    if (time.minute < 10) {
+      paddedMinute = "0" + paddedMinute;
+    }
+    result[field] =`${adjustedHour}:${paddedMinute} ${ampm}`;
+    this.participantForm.patchValue(result);
+  }
+  validateHour(hour: number): void {
+    if (hour > 24 || hour < 0) {
+      throw "Invalid hour";
+    }
+  }
+
+  //Convert an hour in 24-hour time to AM/PM 12-hour time
+  convertTo12Hour(hour: number): number {
+    this.validateHour(hour);
+    if (hour > 12) {
+      return hour - 12;
+    } else if (hour <= 12) {
+      return hour;
+    }
+  }
+
+  convertTo24Hour(hour: number, pm: boolean): number {
+    this.validateHour(hour);
+    if (hour < 12) {
+      return hour + 12;
+    } else { //hour === 12
+      return hour;
+    }
+  }
+
+  prettyTime(startOrEnd: string,info: any): void {
+    console.log("info",info);
+
+    if (startOrEnd === "start") {
+      const startTime = parseTime(info.startTime) || { hour: 0 };
+      console.log("startTime",startTime);
+      if (Object.keys(startTime).length === 2) { //check that start time is a valid time
+        /* Here's the logic for this:
+        * The start time is most likely to be in the morning.  But in some cases, it could also be in the afternoon.
+        * Most start times will be 6-11 AM (inclusive) or 12-5 PM.
+        * For inputs to the start time field that are both a valid time and that do not already have an AM/PM designation, hours 6-11 will have an AM appended, while hours 12-5 will have PM appended.
+        * The end time logic is similar to the above, but with different ranges: 12-7 become PM, 8-11 become AM.
+        * There's also a validation that the end time must be later than the start time.  Time durations that seem illogical (> 8 hours) will also be flagged for manual review by the user.
+        */
+        const timeString: string = "" + info.startTime;
+        console.log("info.startTime is",timeString);
+
+        if (this.timeIncludesAMPM((timeString))) { //just reformat time
+          this.updateTimeField(startTime,"startTime");
+        } else if (startTime.hour <= 6 || startTime.hour >= 12) {
+          this.updateTimeField({hour: this.convertTo24Hour(startTime.hour, true), minute: startTime.minute },"startTime");
+        } else if (startTime.hour > 6 && startTime.hour < 12) {
+          this.updateTimeField({hour: this.convertTo12Hour(startTime.hour), minute: startTime.minute }, "startTime");
         }
-        if (startTime.minute < 10) {
-          paddedMinute = "0" + paddedMinute;
+      }
+    } else if (startOrEnd === "end"){
+      const endTime = parseTime(info.endTime) || { hour: 0 };
+      console.log("endTime",endTime);
+      if (Object.keys(endTime).length === 2) { //check that end time is a valid time
+        const timeString: string = "" + info.endTime;
+        console.log("info.endTime is",timeString);
+
+        if (this.timeIncludesAMPM((timeString))) { //just reformat time
+          this.updateTimeField(endTime,"endTime");
+        } else if (endTime.hour <= 7 || endTime.hour >= 12) {
+          this.updateTimeField({hour: this.convertTo24Hour(endTime.hour, true), minute: endTime.minute },"endTime");
+        } else if (endTime.hour > 7 && endTime.hour < 12) {
+          this.updateTimeField({hour: this.convertTo12Hour(endTime.hour), minute: endTime.minute }, "endTime");
         }
-        this.participantForm.patchValue({ startTime: `${adjustedHour}:${paddedMinute} ${ampm}`})
-      } else if (startTime.hour <= 6 || startTime.hour > 12) {
-        this.participantForm.patchValue({ startTime: `${startTime.hour}:${startTime.minute} PM`});
       }
     }
     //   if (startTime.hour <= 6 && this.participant.startTime.length >= 3 && x.startTime.indexOf("am") === -1 && x.startTime.indexOf("a") === -1) {
@@ -105,4 +160,9 @@ export class ParticipantFormComponent implements OnInit {
   identifier(key: string): any {
     return `${key}${this.workdayNum}-${this.partNum}`;
   }
+}
+
+export class Time {
+  hour: number
+  minute: number
 }
