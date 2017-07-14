95fcc174235b553c54272d3bc0ee5a9965663a5c
Evan Strat <evan10s@users.noreply.github.com>
2017-07-13 23:12:12 -0400

    Some changes to tests; the tests probably don't pass though because they are not written correctly/well

--- src/app/participant-form/participant-form.component.spec.ts
+++ src/app/participant-form/participant-form.component.spec.ts
@@ -22,4 +22,7 @@ describe('ParticipantFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should have validateTime that returns true for times in the form of "h:mm a"',() => {
+    expect(component.validateTime("6:45 PM")).toBeTruthy();
+  })
 });
